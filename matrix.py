b5d243043f90a9b443d6bb52ee0b5f60010c7c85
xzolenr6 <xzolenr6@gmail.com>
2025-01-23 14:49:12 +0000

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -24,8 +24,8 @@ os.makedirs(BASE_DIR, exist_ok=True)
 API_URL = "https://dev.back.matrixcrm.ru/api/v1/AI/servicesByFilters"
 
 # Yandex Cloud конфигурация
-YANDEX_FOLDER_ID = "AQVNw5Kg0jXoaateYQWdSr2k8cbst_y4_WcbvZrW"
-YANDEX_API_KEY = "b1gnq2v60fut60hs9vfb"
+YANDEX_FOLDER_ID = "b1gnq2v60fut60hs9vfb"
+YANDEX_API_KEY = "AQVNw5Kg0jXoaateYQWdSr2k8cbst_y4_WcbvZrW"
 
 logger.info("Загрузка модели векторного поиска...")
 search_model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
